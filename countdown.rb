0ec699c0a210ccca0bec7ac07b73ecf2d4cbb195
Albert LaQue <albertlaque@gmail.com>
2019-11-11 07:27:07 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
-#write your code here
+#needs to have an integer as an argument 
+#the integer needs to be counted down to 0 
+#each iteration should be "#{number} SECOND(S)!"
+#method should return "HAPPY NEW YEAR!"
 
-def countdown
+integer = 10 
+def countdown(integer)
+  while integer < 11 
+  puts "#{integer} SECOND(S)!"
+  integer -= 1 
+  puts "HAPPY NEW YEAR!"
 end
+end
+countdown(integer)
